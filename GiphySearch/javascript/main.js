627791e4c7c151d3d7b1f9354125a50c835410c3
Mashfir Mohiuddin <mashfir@stripe.com>
2020-06-27 15:15:01 -0400

    added GIPHY API call

--- GiphySearch/javascript/main.js
+++ GiphySearch/javascript/main.js
@@ -7,9 +7,33 @@ document.querySelector(".js-go").addEventListener('click', function() {
 
 });
 
+document.querySelector(".js-userinput").addEventListener('keyup', function(e) {
+
+    var input = document.querySelector('input').value;
+
+    // 'Enter' has keyCode 13
+    if(e.which === 13) {
+        pushToDOM(input);
+    }
+
+});
+
 /* 2. API handling */
 
+var url = 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=NgCTfkDrio3ySlBzktPUx6L67J2bRJeL';
 
+// AJAX Request
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open('GET', url);
+GiphyAJAXCall.send();
+
+GiphyAJAXCall.addEventListener('load', function(e) {
+
+    // your callback events go here 
+    var data = e.target.response;
+    console.log(data);
+
+});
 
 /* 3. show GIFs */
 function pushToDOM(input) {
