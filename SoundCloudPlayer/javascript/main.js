f98974a7fa79e50d5388e98392aba68fb6ed20fb
Mashfir Mohiuddin <mashfir@stripe.com>
2020-06-28 17:45:00 -0400

    created soundcloud api class

--- SoundCloudPlayer/javascript/main.js
+++ SoundCloudPlayer/javascript/main.js
@@ -4,16 +4,26 @@
 
 /* Query Soundcloud API */
 
-SC.initialize({
-    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+var SoundCloudAPI = {};
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-    q: 'buskers'
-}).then(function(tracks) {
-    console.log(tracks);
-});
+// wrap initialization in anonymous function of Class object
+SoundCloudAPI.init = function() {
+    SC.initialize({
+        client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+    });
+}
+
+// wrap get tracks in anonymous function of Class object
+SoundCloudAPI.getTrack = function(inputValue) {
+    SC.get('/tracks', {
+        q: inputValue
+    }).then(function(tracks) {
+        console.log(tracks);
+    });
+}
+
+SoundCloudAPI.init();
+SoundCloudAPI.getTrack("Rilo Kiley");
 
 /* Display cards */
 
