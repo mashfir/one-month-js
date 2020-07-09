44f783dd5e38156645469ccf063ba270ab593e80
Mashfir Mohiuddin <mashfir@stripe.com>
2020-07-08 20:18:52 -0400

    added dynamic card content

--- SoundCloudPlayer/javascript/main.js
+++ SoundCloudPlayer/javascript/main.js
@@ -19,65 +19,66 @@ SoundCloudAPI.getTrack = function(inputValue) {
         q: inputValue
     }).then(function(tracks) {
         console.log(tracks);
+        SoundCloudAPI.renderTracks(tracks);
     });
 }
 
 SoundCloudAPI.init();
 SoundCloudAPI.getTrack("Rilo Kiley");
 
-SoundCloudAPI.renderTracks = function() {
-
-    var card = document.createElement('div');
-    card.classList.add('card');
-
-    var image = document.createElement('div');
-    image.classList.add('image');
-
-    var artwork = document.createElement('img');
-    artwork.classList.add('image_img');
-    artwork.src = '#';
-
-    var content = document.createElement('div');
-    content.classList.add('content');
-
-    var header = document.createElement('div');
-    header.classList.add('header');
-
-    var link = document.createElement('a');
-    link.href = '#';
-    link.target = '_blank';
-    link.innerHTML = 'Rilo Kiley';
-
-    var footer = document.createElement('div');
-    footer.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
-
-    var icon = document.createElement('i');
-    icon.classList.add('add', 'icon');
-
-    var subtitle = document.createElement('span');
-    subtitle.innerHTML = 'Add to playlist';
-
-    var searchResults = document.querySelector('.js-search-results');
-
-    card.appendChild(image);
-    image.appendChild(artwork);
-
-    card.appendChild(content);
-    content.appendChild(header);
-    header.appendChild(link);
-
-    card.appendChild(footer);
-    footer.appendChild(icon);
-    footer.appendChild(subtitle);
-
-    searchResults.appendChild(card);
-}
-
-SoundCloudAPI.renderTracks();
-
 /* Display cards */
 
+SoundCloudAPI.renderTracks = function(tracks) {
 
+    tracks.forEach(function(track) {
+
+        var card = document.createElement('div');
+        card.classList.add('card');
+
+        var image = document.createElement('div');
+        image.classList.add('image');
+
+        var artwork = document.createElement('img');
+        artwork.classList.add('image_img');
+        artwork.src = track.artwork_url || 'https://lorempixel.com/100/100/abstract/';
+
+        var content = document.createElement('div');
+        content.classList.add('content');
+
+        var header = document.createElement('div');
+        header.classList.add('header');
+
+        var link = document.createElement('a');
+        link.href = track.permalink_url;
+        link.target = '_blank';
+        link.innerHTML = track.title;
+
+        var footer = document.createElement('div');
+        footer.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+        var icon = document.createElement('i');
+        icon.classList.add('add', 'icon');
+
+        var subtitle = document.createElement('span');
+        subtitle.innerHTML = 'Add to playlist';
+
+        var searchResults = document.querySelector('.js-search-results');
+
+        card.appendChild(image);
+        image.appendChild(artwork);
+
+        card.appendChild(content);
+        content.appendChild(header);
+        header.appendChild(link);
+
+        card.appendChild(footer);
+        footer.appendChild(icon);
+        footer.appendChild(subtitle);
+
+        searchResults.appendChild(card);
+
+    });
+}
 
 /* Add to playlist and play */
 
